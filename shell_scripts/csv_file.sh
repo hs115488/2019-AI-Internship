9054ab06e83e8691c7275a77bfecd34c6b46f820
Haresh Singh <haresh.singh.97@gmail.com>
2020-05-26 23:13:28 -0400

    Create csv_file.sh

---
+++ shell_scripts/csv_file.sh
@@ -0,0 +1,25 @@
+#!/bin.bash
+
+
+cd Users/njcu/Downloads/AutomaticStockTrading/
+
+file = "/Stocks.csv"
+file1 = "/Output.csv"
+sed -e "s/ /,/g" $file 
+
+[[ -f $file ]] || echo "File does not exist. Please try again"
+
+date = awk '{print $1}' $file
+close = awk {print $4}  $file
+
+awk -F '{
+          getline file1 < $file; print file1, $1, $4
+        }' OFS=, $file
+        
+cat $file1
+
+echo "Your file is complete"
+echo "It is located at $pwd"
+echo "Goodbye"
+
+exit
